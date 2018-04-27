f782b05d8d872e71aca7d7b17343ae19437cf2d1
Unknown <kakoty.kaustabh@gmail.com>
2018-04-28 01:34:45 +0530

    fully functional greedy recommender
    added recommender class

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
-from dataset import Graph
 import numpy as np 
+from recommender import Recommender
+from dataset import Graph
 import argparse
 
 ap = argparse.ArgumentParser()
@@ -8,6 +9,10 @@ ap.add_argument("-r", "--ratings_path", required = True, help = "Path to ratings
 
 args = vars(ap.parse_args())
 
-
-
+class User:
+    userIdgen = 300
+    
+    def __init__(self, name):
+        self.name = name
+        self.userId = User.userIdgen + 1
 

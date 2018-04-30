bea9bd6dad44a2c35aec460836568bf7d45bc513
Unknown <kakoty.kaustabh@gmail.com>
2018-04-30 11:17:34 +0530

    minor changes
    file handling

--- user.py
+++ user.py
@@ -1,22 +1,65 @@
 import numpy as np 
-from recommender import Recommender
-from dataset import Graph
+# from recommender import Recommender
+# from dataset import Graph
+# from search import searchTrie
 import argparse
 import operator
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--movie_path", required = True, help = "Path to movies.csv directory")
-ap.add_argument("-r", "--ratings_path", required = True, help = "Path to ratings.csv directory")
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-m", "--movie_path", required = True, help = "Path to movies.csv directory")
+# ap.add_argument("-r", "--ratings_path", required = True, help = "Path to ratings.csv directory")
 
-args = vars(ap.parse_args())
+# args = vars(ap.parse_args())
 
 class User:
-    userIdgen = 300
     
-    def __init__(self, name):
+    f = open("user.txt", 'r')
+    userIdgen = int(f.read())
+    f.close()
+    
+    def __init__(self, name, userId = None, new = True):
         self.name = name
-        self.userId = User.userIdgen + 1
 
-    def chooseMovie(self, movieDict):
-        for i in range(len(movieDict)):
-            print(movieDict)
+        if(new):
+            self.userId = User.userIdgen + 1
+            User.userIdgen = User.userIdgen + 1
+            
+            f = open('user.txt', 'w')
+            f.seek(0)
+            f.write(str(User.userIdgen))
+            f.truncate()
+            f.close()
+
+        else:
+            self.userId = userId
+
+    def rateMovie(self, movie):
+        print(movie + "-- rate on a scale of 1 - 5: ")
+    
+    # def searchMovie(self, searchTrieObj):
+
+
+def main():
+    
+    print("")
+    print("Are you a new user [y/n]: ")
+
+    a = str(input())
+
+    if(a == 'y' or a == 'Y'):  
+        print("Enter your name: ")
+        name = str(input())
+        user = User(name)
+    
+    else:
+        print("Enter your userId: ")
+        print("Enter your name: ")
+        name = str(input())
+        userId = int(input())
+        user = User(name, userId, new = False)
+
+    
+
+
+if __name__ == '__main__':
+    main()
